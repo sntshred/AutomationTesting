12fa9d4f4dfe792522ae123d9dfb97498a5464e9
Santosh Chavan <santosh.aryan9@hotmail.com>
2018-02-15 16:45:11 -0500

    devils 90

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-var sum = function(a, b) {
+function sum(a, b) {
   return a + b;
-};
-
-function somemore() {
-  var x = this.sum + 3;
-  return x;
 }
+
 module.exports = sum;
-module.exports = somemore;
--- app.test.js
+++ app.test.js
@@ -3,7 +3,3 @@ const sum = require("./app");
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
-
-test("Depending up on Sum functio", () => {
-  expect(somemore()).toBe(9);
-});
